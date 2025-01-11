be17d854484fc22641fcefe895e0de1a1062434f
atikahussain12 <hussainatika12@gmail.com>
2025-01-11 10:29:34 -0500

    new changes

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+
+
+    return(
+        <header>
+            <h1>SHEHACKS WEBSITE</h1>
+        </header>
+    );
+}
+
+export default Header
--- vtv.py
+++
@@ -1,7 +0,0 @@
-import gradio as gr
-import assemblyai as aai
-from translate import Translator
-from elevenlabs import VoiceSettings
-from elevenlabs.client import ElevenLabs
-import uuid
-from pathlib import Path
