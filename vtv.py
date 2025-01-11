4d79818eb1abff18bad6e834f5b50e884536a8c5
atikahussain12 <hussainatika12@gmail.com>
2025-01-11 08:38:14 -0500

    commit-1

---
+++ vtv.py
@@ -0,0 +1,7 @@
+import gradio as gr
+import assemblyai as aai
+from translate import Translator
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
+import uuid
+from pathlib import Path
